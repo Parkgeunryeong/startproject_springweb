2d85e7182713a2b0318f193ccca3efbd136dc233
Parkgeunryeong <kkgk7@naver.com>
2025-04-23 14:01:31 +0900

    4/24

---
+++ SpaceWeb/src/main/java/com/springboot/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+	@GetMapping("/space")
+	public String space() {
+		return "space";
+	}
+	
+	
+	
+
+}
